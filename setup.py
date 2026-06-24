83d10a8cb8564f009ebb33da1a1b6a3d84b087ff
Reguntarajasri <rajasriregunta124@gmail.com>
2026-06-24 18:03:02 +0530

    reqirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Rajasri Regunta",
+    email="rajasriregunta124@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
